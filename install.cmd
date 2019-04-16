94cd67fb7d6329c91625d11befab095c89bda87b
Yatao Li <yatli@microsoft.com>
2019-04-17 01:08:19 +0800

    Expand-Archive does not extract pses release package

--- install.cmd
+++ install.cmd
@@ -8,18 +8,21 @@ $file = "PowerShellEditorServices.zip"
 
 $releases = "https://api.github.com/repos/$repo/releases"
 
-Write-Host Determining latest release
+Write-Host Determining latest release...
 $tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
 
 $download = "https://github.com/$repo/releases/download/$tag/$file"
-$zip = "pses.zip"
 
 new-item -Name $dir -ItemType directory -Force
 Write-Host Dowloading $tag
-Invoke-WebRequest $download -Out $zip
+Invoke-WebRequest $download -Out $file
+$file = Get-Item $file
 
-Write-Host Extracting release files
-Expand-Archive $zip -Force -ErrorAction SilentlyContinue
+Write-Host Extracting release files...
 
-Remove-Item $zip -Force
+$shell = new-object -com shell.application
+$zip = $shell.NameSpace($file.FullName)
+foreach($item in $zip.items()) { $shell.Namespace($file.DirectoryName).copyhere($item) }
+
+Remove-Item $file -Force
 Write-Host install completed.
