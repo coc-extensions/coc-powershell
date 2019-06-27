e041d51604e0b4bc344a76b6149b07c8cc1de478
Tyler Leonhardt <tylerl0706@gmail.com>
2019-06-27 09:21:05 -0700

    Update build and enable pack in build.ps1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,14 @@
 param(
+    # Determines if we run `npm pack`.
+    [Parameter()]
+    [switch]
+    $Pack,
+
+    # Determines if we run the tests.
+    [Parameter()]
+    [switch]
+    $Test,
+
     # Runs the watch npm command instead.
     [Parameter()]
     [switch]
@@ -13,6 +23,15 @@ npm install
 
 if ($Watch.IsPresent) {
     npm run watch
+    return
 } else {
     npm run compile
+}
+
+if ($Test.IsPresent) {
+    # We would run the tests here.
+}
+
+if ($Pack.IsPresent) {
+    npm pack
 }
